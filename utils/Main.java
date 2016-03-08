c063bdb673df153093171bf479f8ab7ef8378393
KerDam <kerdamien@gmail.com>
2016-03-08 14:53:20 +0100

    correction de bugs, et test de connection avec le Welcome server réussi

--- utils/Main.java
+++ utils/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		/* Test de pair avec le serveur welcom */
-//		Pair pair = new Pair();
-		Communication com = new Communication();
-//		com.send("yo:5:192.168.0.21", "localhost");
+		Pair pair = new Pair();
+//		Communication com = new Communication(pair);
+//		WelcomeServer ws = new WelcomeServer();
+		pair.communication.send("yo:5:192.168.18.21", "localhost", 8000);
 //		com.askHash();
 		/* Test de connexion avec serveur hash et welcome*/
 		
